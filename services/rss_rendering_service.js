7dd4747de606f8953cd471761f956a3f26dd882b
Logan Genet <logan.genet@careerbuilder.com>
2016-03-23 16:36:32 -0400

    Extended error handling

--- services/rss_rendering_service.js
+++ services/rss_rendering_service.js
@@ -1,8 +1,8 @@
 var moment = require('moment');
 moment().format();
 module.exports = function RSSRenderingServiceModule(pb) {
-  const WORDS_PER_PREVIEW = 24; 
-  
+  const WORDS_PER_PREVIEW = 24;
+
   function RSSRenderingService(options){
     if (options) {
       this.site = options.site || pb.SiteService.GLOBAL_SITE;
@@ -22,11 +22,12 @@ module.exports = function RSSRenderingServiceModule(pb) {
 
   RSSRenderingService.prototype.render = function(cb) {
     var self = this;
+    var jts = null;
     getFeed(self, function(err, feed) {
       if(!err && feed[0] && feed[0].item) {
         var post = feed[0].item[0];
         var posted = getTimeFromNow(post.pubDate[0]);
-        var jts = new pb.TemplateService({ls:self.ls, site:self.site});
+        jts = new pb.TemplateService({ls:self.ls, site:self.site});
         jts.reprocess = false;
         jts.registerLocal('blog_url', feed[0].link[0]);
         jts.registerLocal('post_text', getPostPreview(post.description[0]));
@@ -34,8 +35,8 @@ module.exports = function RSSRenderingServiceModule(pb) {
         jts.registerLocal('post_posted', posted);
         jts.load('elements/rss', cb);
       } else {
-        pb.log.error('rssfeeder error: ' + err);
-        var jts = new pb.TemplateService({ls:self.ls, site:self.site});
+        pb.log.error('Rss Feeder Encountered an Error: [error] ' + err + ' [Feed] ' + feed);
+        jts = new pb.TemplateService({ls:self.ls, site:self.site});
         jts.reprocess = false;
         jts.registerLocal('blog_url', '');
         jts.registerLocal('post_text', '^loc_RSS_ERROR^');
@@ -45,14 +46,14 @@ module.exports = function RSSRenderingServiceModule(pb) {
       }
     });
   };
-  
+
   function getPostPreview(text) {
     var text = text.replace(/(<([^>]+)>)/ig,"");
     var words = text.split(' ');
     words.length = WORDS_PER_PREVIEW;
     return words.join(' ') + '...';
   }
-  
+
   function getTimeFromNow(date) {
     var parsedDate = moment(date, 'YYYY-MM-DDTHH:mm:ss.SSSZ', 'en');
     if(parsedDate.isValid()) {
@@ -60,12 +61,12 @@ module.exports = function RSSRenderingServiceModule(pb) {
     }
     return '';
   }
-  
+
   function getFeed(self, cb) {
     var RssService = pb.PluginService.getService('rssFeederService', 'pencilblue_rssfeeder', self.site);
     var rssService = new RssService({site:self.site});
     rssService.getFeed(cb);
   }
-  
+
   return RSSRenderingService;
 };
